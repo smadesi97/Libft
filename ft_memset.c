5e6a9507b4781a069d257e1b07fee7153ce9ca26
Sanelisiwe MADESI <smadesi@c5r8s2.wethinkcode.co.za>
2019-05-24 15:08:13 +0200

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,11 @@
 /*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 11:28:37 by smadesi           #+#    #+#             */
-/*   Updated: 2019/05/22 09:19:39 by smadesi          ###   ########.fr       */
+/*   Updated: 2019/05/24 15:07:13 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h> 
+#include <string.h>
 
 void	*ft_memset(void *b, int c, size_t len)
 {
@@ -24,29 +23,4 @@ void	*ft_memset(void *b, int c, size_t len)
 		len--;
 	}
 	return (b);
-}
-
-int	main()
-{
-  //Basic string to test
-	char str[50] = "Testing this sh*t !\0";
-	char str2[50] = "Testing this sh*t !\0";
-	//replace by
-	char replace = '-';
-	//on X c
-	int nbr = 5;
-	
-	//Display result
-	printf("\x1b[31m-----ORIGINAL MEMSET : %c : %d-----\n\x1b[0m", replace, nbr);
-	printf("\x1b[32mOriginal : %s\n\x1b[0m", str);
-	printf("\x1b[32mMemset   : %s\n\x1b[0m", (char *)memset(str, replace, nbr));
-	printf("\x1b[31m---------------------------------\n\x1b[0m");
-	printf("\x1b[31m\n-----TEST FT_MEMSET : %c : %d------\n\x1b[0m", replace, nbr);
-	printf("\x1b[32mOriginal : %s\n\x1b[0m", str2);
-	printf("\x1b[32mft_memset: %s\n\x1b[0mSIZE OR SEGFAULT ON EACH FNC
-	if (ft_strcmp(str, str2))
-		printf("\x1b[32mSUCCESS GG\n\x1b[0m");
-	else
-		printf("\x1b[31mERROR ORIGINAL - TEST DIFFERENT\n\x1b[0m");
-	return (0);	
 }
