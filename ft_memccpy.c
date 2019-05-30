534f46baa18bb63c39e684f2dd770cb6ee169575
Sanelisiwe MADESI <smadesi@c5r8s2.wethinkcode.co.za>
2019-05-30 15:57:09 +0200

    ccopy

---
+++ ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 10:18:34 by smadesi           #+#    #+#             */
+/*   Updated: 2019/05/30 10:37:35 by smadesi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+		unsigned const char *t_s;
+		unsigned char		*t_d;
+		size_t				i;
+
+		i = 0;
+		t_s = src;
+		t_d = dst;
+		while (i < n)
+		{
+				if (t_s[i] == c)
+						break;
+				t_d[i] = t_s[i];
+				i++;
+		}
+		return (dst);
+}
