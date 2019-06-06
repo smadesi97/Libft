1de28d76389c35ccda7293aa0750ec318a20632c
Sanelisiwe MADESI <smadesi@c5r8s9.wethinkcode.co.za>
2019-06-06 16:56:23 +0200

    part1

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,13 +6,13 @@
 /*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:51:03 by smadesi           #+#    #+#             */
-/*   Updated: 2019/06/03 17:39:24 by smadesi          ###   ########.fr       */
+/*   Updated: 2019/06/06 16:51:59 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 	size_t	j;
@@ -23,11 +23,12 @@ char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
 	j = 0;
 	while (s1[i])
 		i++;
-	while (j <= (n - 1) && s[j])
+	while ((j < n) && s[j])
 	{
-		s1[i + j] = s[j];
+		s1[i] = s[j];
 		j++;
+		i++;
 	}
-	s1[i + j] = '\0';
-	return ((char*)s1);
+	s1[i] = '\0';
+	return (s1);
 }
