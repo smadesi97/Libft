5404a04bd874d33dc7afbd1572504d5f71d2a927
Sanelisiwe MADESI <smadesi@c5r8s9.wethinkcode.co.za>
2019-06-07 12:59:41 +0200

    part1

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,18 +6,18 @@
 /*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:33:34 by smadesi           #+#    #+#             */
-/*   Updated: 2019/06/03 14:23:56 by smadesi          ###   ########.fr       */
+/*   Updated: 2019/06/07 12:59:15 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+int		ft_strcmp(const char *s1, const char *s2)
 {
-	int i;
+	unsigned int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' && s1[i] == s2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
