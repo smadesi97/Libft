1f078a19d64019d984166c56b9d2d74291c48ee7
Sanelisiwe MADESI <smadesi@c5r8s9.wethinkcode.co.za>
2019-06-10 17:22:54 +0200

    part1

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 08:46:31 by smadesi           #+#    #+#             */
-/*   Updated: 2019/06/03 14:11:17 by smadesi          ###   ########.fr       */
+/*   Updated: 2019/06/10 17:22:12 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,17 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 	size_t			i;
+	int				res;
 
 	i = 0;
 	str1 = (unsigned char*)s1;
 	str2 = (unsigned char*)s2;
-	while (str1[i] == str2[i] && str1[i] && i < n)
+	while (i < n)
+	{
+		res = str1[i] - str2[i];
+		if (res != 0)
+			return (res);
 		i++;
-	if (!(i == n))
-		return (str1[i] - str2[i]);
-	return (str1[i - 1] - str2[i - 1]);
+	}
+	return (0);
 }
