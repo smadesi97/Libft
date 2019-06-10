90c0384d7c11cce4b1e881231c002c6adb02e997
Sanelisiwe MADESI <smadesi@c5r8s9.wethinkcode.co.za>
2019-06-10 17:48:20 +0200

    part 1

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 09:20:25 by smadesi           #+#    #+#             */
-/*   Updated: 2019/06/06 11:05:54 by smadesi          ###   ########.fr       */
+/*   Updated: 2019/06/10 17:48:07 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	i = 0;
 	if (!s || !f)
 		return (0);
-	frsh = ft_strnew(ft_strlen((const char *)s));
+	fresh = ft_strnew(ft_strlen((const char *)s));
 	if (!fresh)
 		return (0);
 	while ((char)s[i])
