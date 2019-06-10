94abaf4d26fb7b88b5c476a0f9f35a478d5fa32e
Sanelisiwe MADESI <smadesi@c5r8s9.wethinkcode.co.za>
2019-06-10 17:38:51 +0200

    part 1

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 12:08:05 by smadesi           #+#    #+#             */
-/*   Updated: 2019/06/03 14:25:07 by smadesi          ###   ########.fr       */
+/*   Updated: 2019/06/10 17:38:14 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,18 @@
 
 char	*ft_strdup(const char *s1)
 {
-	int i;
-	int len;
-	char*str;
+	char	*str;
+	size_t	i;
 
-	len = 0;
-	while (s1[len])
-		len++;
-	str = (char*)malloc(sizeof(*str) * (len + 1));
 	i = 0;
-	while (i < len)
+	str = (char*)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
 	{
 		str[i] = s1[i];
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
