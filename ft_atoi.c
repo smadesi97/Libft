b822036b9be0328c042692d846516e45cc7d6a1e
Sanelisiwe MADESI <smadesi@c5r8s9.wethinkcode.co.za>
2019-06-10 17:10:12 +0200

    part1

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,17 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
 /*   Created: 2019/06/03 12:51:33 by smadesi           #+#    #+#             */
-/*   Updated: 2019/06/03 16:17:42 by smadesi          ###   ########.fr       */
-=======
-/*   Created: 2019/05/24 09:56:20 by smadesi           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/05/31 09:49:02 by smadesi          ###   ########.fr       */
-=======
-/*   Updated: 2019/05/31 09:41:11 by smadesi          ###   ########.fr       */
->>>>>>> 8bbae14df541411bb02d5c0986c11e9290897894
->>>>>>> 7a532e5fa5dfb99a775c8f84732f247a86757958
+/*   Updated: 2019/06/10 17:09:48 by smadesi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +14,9 @@
 
 int	ft_atoi(const char *str)
 {
-<<<<<<< HEAD
 	int				res;
 	int				sign;
-	unsigned int	i;
-=======
-<<<<<<< HEAD
-	int				res;
-	int				sign;
-	unsigned int	i;
-=======
-	int		res;
-	int		sign;
-	unsigned int i;
->>>>>>> 8bbae14df541411bb02d5c0986c11e9290897894
->>>>>>> 7a532e5fa5dfb99a775c8f84732f247a86757958
+	int				i;
 
 	res = 0;
 	sign = 1;
